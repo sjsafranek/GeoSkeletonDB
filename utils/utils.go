89717f569b639e54151030990a52ef0475d452ec
stefan safranek <stefan@stefan.stefan>
2017-03-22 21:37:26 -0700

    refactor

---
+++ utils/utils.go
@@ -0,0 +1,138 @@
+package utils
+
+import (
+	"bytes"
+	"compress/flate"
+	crand "crypto/rand"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/url"
+	"time"
+)
+
+const _letters string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// NewUUID generates and returns url friendly uuid
+// Source: http://play.golang.org/p/4FkNSiUDMg
+// @returns string
+// @returns error
+func NewUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(crand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	// return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
+	return fmt.Sprintf("%x%x%x%x%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+}
+
+// NewUUID2 generates and returns a uuid
+// @returns string
+// @returns error
+func NewUUID2() (string, error) {
+	b := make([]byte, 16)
+	n, err := io.ReadFull(crand.Reader, b)
+	if n != len(b) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	b[8] = b[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	b[6] = b[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
+}
+
+// NewAPIKey generates and returns an apikey of desired length
+// @param int length of apikey
+// @returns string
+func NewAPIKey(n int) string {
+	s := ""
+	for i := 1; i <= n; i++ {
+		s += string(_letters[rand.Intn(len(_letters))])
+	}
+	return s
+}
+
+// stringInSlice loops through a []string and returns a bool if string is found
+// @param a {string} string to find
+// @param list {[]string} array of strings to search
+// @returns bool
+func StringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+// sliceIndex loops through a []string and returns the index of a string
+// Description:
+//		Loops through array of strings
+//		Checks each string in array for match
+//		If string match occurs returns index
+// @param value {string} string to find
+// @param slice {[]string} array of strings to search
+// @returns int
+func SliceIndex(value string, slice []string) int {
+	for p, v := range slice {
+		if v == value {
+			return p
+		}
+	}
+	return -1
+}
+
+// Compression
+// Source: https://github.com/schollz/gofind/blob/master/utils.go#L146-L169
+//         https://github.com/schollz/gofind/blob/master/fingerprint.go#L43-L54
+// Description:
+//		Compress and Decompress bytes
+func CompressByte(src []byte) []byte {
+	compressedData := new(bytes.Buffer)
+	Compress(src, compressedData, 9)
+	return compressedData.Bytes()
+}
+
+func DecompressByte(src []byte) []byte {
+	compressedData := bytes.NewBuffer(src)
+	deCompressedData := new(bytes.Buffer)
+	Decompress(compressedData, deCompressedData)
+	return deCompressedData.Bytes()
+}
+
+func Compress(src []byte, dest io.Writer, level int) {
+	compressor, _ := flate.NewWriter(dest, level)
+	compressor.Write(src)
+	compressor.Close()
+}
+
+func Decompress(src io.Reader, dest io.Writer) {
+	decompressor := flate.NewReader(src)
+	io.Copy(dest, decompressor)
+	decompressor.Close()
+}
+
+// isUrl
+// https://www.socketloop.com/tutorials/golang-how-to-validate-url-the-right-way
+func IsUrl(str string) bool {
+	fmt.Println(str)
+	var validURL bool
+	_, err := url.Parse(str)
+	if err != nil {
+		fmt.Println(err)
+		validURL = false
+	} else {
+		validURL = true
+	}
+	return validURL
+}
