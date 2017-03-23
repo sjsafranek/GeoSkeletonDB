06b915f72aed0274cf930b82863d8957315fd1ac
stefan safranek <stefan@stefan.stefan>
2017-03-23 08:42:06 -0700

    GeoTsDB

--- timeseries.go
+++ timeseries.go
@@ -1,20 +1,62 @@
-package geo_skeleton
+	package geo_skeleton
 
 import "github.com/sjsafranek/DiffDB/diff_store"
-import "github.com/sjsafranek/DiffDB/diff_db"
+import "github.com/sjsafranek/SkeletonDB"
+// import "github.com/sjsafranek/DiffDB/diff_db"
 
-var diffDb diff_db.DiffDb
+var GeoTsDB GeoTimeseriesDB
 
 func init() {
-	diffDb = diff_db.NewDiffDb("diff.db")
-	diffDb.Init()
+	GeoTsDB = GeoTimeseriesDB{}
+	GeoTsDB.Init()
+}
+
+type GeoTimeseriesDB struct {
+	File string
+	Table    string
+	DB       skeleton.Database
+}
+
+func (self GeoTimeseriesDB) Init() {
+	if !self.DB {
+		self.DB = skeleton.Database{File: self.getFile()}}
+	}
+
+	self.DB.Init()
+
+	conn := self.DB.Connect()
+	defer conn.Close()
+
+	err := self.DB.CreateTable(conn, self.getTable())
+	if nil != err {
+		panic(err)
+	}
+}
+
+func (self GeoTimeseriesDB) getFile() {
+	if "" == self.File {
+		return = "diff.db"
+	}
+	return self.File
+}
+
+func (self GeoTimeseriesDB) getTable() {
+	if "" == self.Table {
+		return = "DiffData"
+	}
+	return self.Table
+}
+
+func (self GeoTimeseriesDB) Select(datasource_id) ([]byte, error) {
+	data, err := self.DB.Select(self.getTable, datasource_id)
+	return data, err
 }
 
 func update_timeseries_datasource(datasource_id string, value []byte) {
 
 	update_value := string(value)
 	var ddata diff_store.DiffStore
-	data, err := diffDb.Load(datasource_id)
+	data, err := GeoTsDB.Select(datasource_id)
 	if nil != err {
 		if err.Error() == "Not found" {
 			// create new diffstore if key not found in database
@@ -36,6 +78,6 @@ func update_timeseries_datasource(datasource_id string, value []byte) {
 	}
 
 	ddata.Name = datasource_id
-	err = diffDb.Save(string(ddata.Name), enc)
+	err = GeoTsDB.Save(string(ddata.Name), enc)
 
 }
